bc9d95ef8536ec08e1577f5d8b900888773df0a7
DASARIRAJU <rdasari77@gmail.com>
2024-12-28 16:32:15 +0530

    test

--- tests/firsttest.spec.js
+++ tests/firsttest.spec.js
@@ -4,7 +4,7 @@ test('First' , async ({page}) => {
 
     await page.goto("https://www.facebook.com/login/");
     const pageTitle = await page.title();
-    console.log("Page title is : " ,pageTitle);
+    console.log("Facebook Page title is : " ,pageTitle);
     await expect(page).toHaveTitle("Log in to Facebook");
     await page.close();
 
