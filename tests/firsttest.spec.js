23256854de1cf0011398d0eefdf81e89fbbc3f40
Raju <rdasari77@gmail.com>
2024-12-28 15:58:34 +0530

    tes

---
+++ tests/firsttest.spec.js
@@ -0,0 +1,12 @@
+const{test,expect} = require('@playwright/test')
+
+test('First' , async ({page}) => {
+
+    await page.goto("https://www.facebook.com/login/");
+    const pageTitle = await page.title();
+    console.log("Page title is : " ,pageTitle);
+    await expect(page).toHaveTitle("Log in to Facebook");
+    await page.close();
+
+}
+)
